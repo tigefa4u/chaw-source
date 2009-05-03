52cb4af86e6c6ee1b2bb39347be59cb528af24ff
gwoo <gwoo@cakephp.org>
2009-05-02 17:07:46 -0700

    updating index.php in master

--- webroot/index.php
+++ webroot/index.php
@@ -81,7 +81,7 @@
 		return;
 	} else {
 		$Dispatcher = new Dispatcher();
-		$Dispatcher->dispatch($url);
+		$Dispatcher->dispatch();
 	}
 	if (Configure::read() > 0) {
 		echo "<!-- " . round(getMicrotime() - $TIME_START, 4) . "s -->";
